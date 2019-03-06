169b25adef8cb311556314028d83dd21f73d9013
visualstudio <visualstudio@122-PC06>
2019-03-06 11:23:05 -0500

    Lab07

--- C0732156-CSD3354-2-Asignment-2/Program.cs
+++ C0732156-CSD3354-2-Asignment-2/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace C0732156_CSD3354_2_Asignment_2
 {
 
+
     namespace DelegatesAndEvents
     {
         public class Program
@@ -25,18 +26,18 @@ namespace C0732156_CSD3354_2_Asignment_2
 
             int Method1(int intMethod1)
             {
-                return intMethod1 * 2;
+                return intMethod1 * 4;
             }
 
             int Method2(int intMethod1)
             {
-                return intMethod1 * 10;
+                return intMethod1 * 20;
             }
 
             public void Method4(MyDelegate myDelegate)
             {
-                int result = myDelegate(10);
-                Console.WriteLine(result);
+                for (int i = 1; i <= 5; i++)
+                    System.Console.Write(myDelegate(i) + " ");
             }
 
             public void Method3()
@@ -49,3 +50,5 @@ namespace C0732156_CSD3354_2_Asignment_2
         }
     }
 }
+
+    
